5cd3aaf2bd00ae019ff43187d38565ebd38e1ffb
Juan Pablo Chavez Arteaga <juanpablo.chavez@training.assuresoft.com>
2023-11-29 01:00:48 -0400

    Endpoint to create users added

--- src/repositories/UserPGRespository.php
+++ src/repositories/UserPGRespository.php
@@ -14,26 +14,6 @@ class UserPGRepository implements UserRepository
         $this->db = DBConnection::getConnection();
     }
 
-    public function getAll(): array {
-        $query = "SELECT id, name, last_name, email, role_id FROM public.user";
-        $stmt = $this->db->prepare($query);
-        $stmt->execute();
-        $users = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-        return $users;
-    }
-
-    public function getById(int $id): array {
-        $query = "SELECT id, name, last_name, email, role_id FROM public.user WHERE id = :id";
-        $stmt = $this->db->prepare($query);
-        $stmt->bindParam(':id', $id);
-        $stmt->execute();
-        $user = $stmt->fetch(\PDO::FETCH_ASSOC);
-        if($user == false){
-            throw new \Exception("User not found");
-        }
-        return $user;
-    }
-
     public function create(User $user): array {
         $query = "INSERT INTO public.user (name, last_name, email, password, role_id) VALUES (:name, :last_name, :email, :password, :role_id)";
         $stmt = $this->db->prepare($query);
@@ -54,31 +34,5 @@ class UserPGRepository implements UserRepository
             'password' => $user['password'],
             'role_id' => $user['role_id']
         ];
-    }
-
-    public function update(User $user): array {
-        $query = "UPDATE public.user SET name = :name, last_name = :last_name, email = :email, password = :password, role_id = :role_id WHERE id = :id";
-        $stmt = $this->db->prepare($query);
-        $stmt->bindParam(':id', $user['id']);
-        $stmt->bindParam(':name', $user['name']);
-        $stmt->bindParam(':last_name', $user['last_name']);
-        $stmt->bindParam(':email', $user['email']);
-        $stmt->bindParam(':password', $user['password']);
-        $stmt->bindParam(':role_id', $user['role_id']);
-        if(!$stmt->execute()) {
-            throw new \Exception($stmt->errorInfo()[2]);
-        }
-        return $user->toArray();
-    }
-
-    public function delete(int $id): array {
-        $user = $this->getById($id);
-        $query = "DELETE FROM public.user WHERE id = :id";
-        $stmt = $this->db->prepare($query);
-        $stmt->bindParam(':id', $id);
-        if(!$stmt->execute()) {
-            throw new \Exception($stmt->errorInfo()[2]);
-        }
-        return $user;
     }
 }
