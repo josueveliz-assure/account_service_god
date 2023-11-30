88735a4f9def53a2f18757e753746f7a0b388a2c
JoDemVel <josuedemetriovelizescobar@gmail.com>
2023-11-30 19:15:23 -0400

    Add User mapper and complete CRUD in UserPGRepository

--- src/repositories/UserRepository.php
+++ src/repositories/UserRepository.php
@@ -6,5 +6,9 @@ use App\Models\User;
 
 interface UserRepository
 {
-    public function create(User $user);
+    public function create(User $user) : User;
+    public function getById(int $id) : User;
+    public function getAll() : array;
+    public function update(User $user) : User;
+    public function delete(int $id) : User;
 }
